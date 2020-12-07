65bd508212f8964012de941492bc9851190b8fac
JongChan <kjc3590@songareeit.com>
2020-12-07 10:01:29 +0900

    createMeet 수정

--- src/main/java/com/toy/mealimeter/meet/controller/MeetController.java
+++ src/main/java/com/toy/mealimeter/meet/controller/MeetController.java
@@ -6,13 +6,13 @@ import com.toy.mealimeter.meet.service.MeetService;
 import com.toy.mealimeter.user.domain.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/meet")
@@ -26,7 +26,7 @@ public class MeetController {
 
     @ApiOperation(value = "방 생성.")
     @PostMapping
-    public ResponseEntity<MeetDto.Res> createMeet(MeetDto.Req req) {
+    public ResponseEntity<MeetDto.Res> createMeet(@RequestBody @ApiParam(name = "MeetDto.Req") @Valid MeetDto.Req req) {
         User user = SecurityService.getUser();
         return ResponseEntity.ok(meetService.createMeet(user, req));
     }
